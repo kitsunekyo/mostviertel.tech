f768caf44c128126a758817229d1c06325c6c5c4
Alex Spieslechner <alexander.spieslechner@gmail.com>
2022-01-04 18:50:52 +0100

    feat: styles

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,10 @@
 import classNames from "classnames";
+import style from "./Button.module.css";
 
 export const Button = ({ children, onClick, className, ...rest }) => {
   return (
     <button
-      className={classNames("button text-sm", className)}
+      className={classNames(style.button, "text-sm", className)}
       onClick={onClick}
       {...rest}
     >
