b7d048da7c95fe6bd86da05dee20c8b2a6bd6d57
Alex Spieslechner <alexander.spieslechner@gmail.com>
2022-01-04 19:04:14 +0100

    feat: optimize avatar image

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,13 +1,21 @@
 import React from "react";
 import Image from "next/image";
 
+import profilePic from "public/images/profile.jpg";
+
 export const Avatar = () => {
   return (
     <div
       className="border-2 border-black inline-block rounded-md overflow-hidden"
       style={{ lineHeight: 0, boxShadow: "var(--shadow-hard)" }}
     >
-      <Image src="/images/profile.jpg" height={130} width={130} alt="alex" />
+      <Image
+        src={profilePic}
+        height={130}
+        width={130}
+        alt="alex"
+        placeholder="blur"
+      />
     </div>
   );
 };
