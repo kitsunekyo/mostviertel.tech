6185354ed91abb8dc35a68b2c3dceb4b22484f6a
Alex Spieslechner <alexander.spieslechner@gmail.com>
2022-01-04 12:15:12 +0100

    feat: sidebar

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 import { Button } from "src/components/Button";
 
-export const Header = () => (
+export const Header = ({ onMenuClicked }) => (
   <header className="border-b border-gray-300">
     <div className="flex items-center contained">
       <Link href="/">
@@ -10,7 +10,8 @@ export const Header = () => (
           <div className="font-semibold">mostviertel.tech</div>
         </a>
       </Link>
-      <nav className="ml-auto flex items-center gap-4 text-sm">
+
+      <nav className="ml-auto items-center gap-4 text-sm hidden md:flex">
         <Link href="/blog">
           <a>Blog</a>
         </Link>
@@ -21,6 +22,26 @@ export const Header = () => (
           <Button>Contact Me</Button>
         </Link>
       </nav>
+
+      <div className="ml-auto">
+        {/* menu icon */}
+        <button onClick={onMenuClicked}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M4 6h16M4 12h8m-8 6h16"
+            />
+          </svg>
+        </button>
+      </div>
     </div>
   </header>
 );
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,10 +1,15 @@
+import { useState } from "react";
 import Head from "next/head";
+import Link from "next/link";
 
 import { Header } from "./Header";
+import { Button } from "./Button";
 
 export const siteTitle = "mostviertel.tech";
 
 export const Layout = ({ children }) => {
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
   return (
     <>
       <Head>
@@ -20,9 +25,57 @@ export const Layout = ({ children }) => {
         <meta name="twitter:card" content="summary_large_image" />
       </Head>
 
-      <Header />
+      <Header onMenuClicked={() => setIsDrawerOpen(true)} />
 
       <main className="flex-grow bg-gray-50">{children}</main>
+
+      {isDrawerOpen && (
+        <>
+          {/* overlay */}
+          <div
+            className="absolute inset-0"
+            style={{ background: "hsla(0, 0%, 0%, 0.8)" }}
+          />
+          {/* drawer */}
+          <div className="absolute inset-0 bg-white" style={{ left: "2rem" }}>
+            <div className="p-4">
+              {/* close button */}
+              <div className="flex">
+                <button
+                  onClick={() => setIsDrawerOpen(false)}
+                  className="ml-auto"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </svg>
+                </button>
+              </div>
+              <nav className="items-center gap-2 text-base flex flex-col pt-20 font-medium">
+                <Link href="/blog">
+                  <a className="p-4 w-full text-center">Blog</a>
+                </Link>
+                <Link href="/uses">
+                  <a className="p-4 w-full text-center">Uses</a>
+                </Link>
+                <Link href="mailto:alexander.spieslechner@gmail.com" passHref>
+                  <Button className="mt-6">Contact Me</Button>
+                </Link>
+              </nav>
+            </div>
+          </div>
+        </>
+      )}
     </>
   );
 };
