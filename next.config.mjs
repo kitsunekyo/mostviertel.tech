48aaefbaa7bd9e852a1d6f6a54e75f3d906548b3
Alex Spieslechner <alexander.spieslechner@gmail.com>
2024-01-01 13:02:02 +0100

    feat: setup markdown and shikiji

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,22 @@
 import nextMDX from "@next/mdx";
+import rehypePrettyCode from "rehype-pretty-code";
+import { mdxAnnotations } from "mdx-annotations";
+
+/** @type {import('rehype-pretty-code').Options} */
+const prettyCodeOptions = {};
+
+const mdxOptions = {
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [mdxAnnotations.recma],
+    rehypePlugins: [[rehypePrettyCode, prettyCodeOptions]],
+    recmaPlugins: [],
+  },
+};
+
+const withMDX = nextMDX(mdxOptions);
 
 /** @type {import('next').NextConfig} */
 const nextConfig = { pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"] };
 
-const withMDX = nextMDX({
-  options: {},
-});
-
-module.exports = withMDX(nextConfig);
+export default withMDX(nextConfig);
