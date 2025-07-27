a9b084657afa3932580fc187cd468b378fc0e85d
Alex Spieslechner <alexander.spieslechner@dynatrace.com>
2025-07-27 18:21:07 +0200

    feat: blog section style update

--- src/app/blog/[...slug]/page.tsx
+++ src/app/blog/[...slug]/page.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import PostLayout from "@/components/PostLayout";
 
-export default async function Page({ params }: { params: { slug: string } }) {
-  const content = await import(`@/_markdown/${params.slug}/page.mdx`);
+export default async function Page({ params }: { params: { slug: string[] } }) {
+  const content = await import(`@/_markdown/${params.slug.join("/")}/page.mdx`);
 
   return (
     <PostLayout frontmatter={content.frontmatter}>
@@ -14,9 +14,11 @@ export default async function Page({ params }: { params: { slug: string } }) {
 export async function generateMetadata({
   params,
 }: {
-  params: { slug: string };
+  params: { slug: string[] };
 }): Promise<Metadata> {
-  const { frontmatter } = await import(`@/_markdown/${params.slug}/page.mdx`);
+  const { frontmatter } = await import(
+    `@/_markdown/${params.slug.join("/")}/page.mdx`
+  );
 
   return {
     title: frontmatter.title,
--- src/components/BlogSection.tsx
+++ src/components/BlogSection.tsx
@@ -1,14 +1,24 @@
 import glob from "fast-glob";
+import path from "node:path";
+
+interface Frontmatter {
+  title?: string;
+  excerpt?: string;
+  date?: string;
+  tags?: string[];
+}
 
 async function getPostsMeta() {
-  const pages = await glob("**/*.mdx", { cwd: "src/_markdown" });
+  const pages = await glob("**/*.mdx", { cwd: "src/_markdown/" });
 
   const pagesWithData = await Promise.all(
     pages.map(async (page) => {
-      const { frontmatter } = await import(`@/_markdown/${page}`);
+      const { frontmatter }: { content: string; frontmatter?: Frontmatter } =
+        await import(`@/_markdown/${page}`);
 
       return {
         frontmatter,
+        pagePath: page,
         href: `/blog/${page.replace(/page\.mdx$/, "")}`,
       };
     }),
@@ -25,11 +35,24 @@ async function getPostsMeta() {
     );
   });
 
-  return sorted;
+  const groupedByYear: {
+    [year: string]: { frontmatter?: Frontmatter; href: string }[];
+  } = {};
+
+  for (let page of sorted) {
+    const year = page.pagePath.split(path.sep)[0];
+    if (!groupedByYear[year]) {
+      groupedByYear[year] = [page];
+    } else {
+      groupedByYear[year].push(page);
+    }
+  }
+
+  return { postsByYear: groupedByYear };
 }
 
 export async function BlogSection() {
-  const posts = await getPostsMeta();
+  const { postsByYear } = await getPostsMeta();
 
   return (
     <div className="py-24 sm:py-32 px-6 lg:px-8">
@@ -49,42 +72,57 @@ export async function BlogSection() {
       </div>
 
       <div className="mx-auto mt-10 max-w-2xl space-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16">
-        {posts.map((post) => (
-          <article
-            key={post.href}
-            className="flex max-w-xl flex-col items-start justify-between"
-          >
-            <div className="flex items-center gap-x-4 text-xs">
-              <time dateTime={post.frontmatter?.date} className="text-gray-500">
-                {post.frontmatter?.date}
-              </time>
-              {post.frontmatter?.tags &&
-              Array.isArray(post.frontmatter.tags) ? (
-                <div className="flex gap-1">
-                  {post.frontmatter.tags.map((tag) => (
-                    <a
-                      key={tag}
-                      className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600"
-                    >
-                      {tag}
-                    </a>
-                  ))}
-                </div>
-              ) : null}
-            </div>
-            <div className="group relative">
-              <h3 className="mt-3 text-lg/6 font-semibold text-gray-900 group-hover:text-indigo-500">
-                <a href={post.href}>
-                  <span className="absolute inset-0" />
-                  {post.frontmatter?.title}
-                </a>
+        {Object.keys(postsByYear)
+          .sort((a, b) => (a > b ? -1 : 0))
+          .map((year) => (
+            <div key={year}>
+              <h3 className="my-3 text-xl/6 font-black text-gray-700 -translate-x-2 tracking-tight mb-8 -skew-y-2 text-balance">
+                {year}
               </h3>
-              <p className="mt-5 line-clamp-3 text-sm/6 text-gray-600">
-                {post.frontmatter?.excerpt}
-              </p>
+
+              <div className="space-y-12">
+                {postsByYear[year].map((post) => (
+                  <article
+                    key={post.href}
+                    className="flex max-w-xl flex-col items-start justify-between"
+                  >
+                    <div className="flex items-center gap-x-4 text-xs">
+                      <time
+                        dateTime={post.frontmatter?.date}
+                        className="text-gray-500"
+                      >
+                        {post.frontmatter?.date}
+                      </time>
+                      {post.frontmatter?.tags &&
+                      Array.isArray(post.frontmatter.tags) ? (
+                        <div className="flex gap-1">
+                          {post.frontmatter.tags.map((tag) => (
+                            <a
+                              key={tag}
+                              className="relative z-10 rounded-full px-2 py-1 font-medium text-gray-600 bg-gray-100"
+                            >
+                              {tag}
+                            </a>
+                          ))}
+                        </div>
+                      ) : null}
+                    </div>
+                    <div className="group relative">
+                      <h4 className="mt-3 text-lg/6 font-semibold text-gray-900 group-hover:text-indigo-500">
+                        <a href={post.href}>
+                          <span className="absolute inset-0" />
+                          {post.frontmatter?.title}
+                        </a>
+                      </h4>
+                      <p className="mt-5 line-clamp-3 text-sm/6 text-gray-600">
+                        {post.frontmatter?.excerpt}
+                      </p>
+                    </div>
+                  </article>
+                ))}
+              </div>
             </div>
-          </article>
-        ))}
+          ))}
       </div>
     </div>
   );
